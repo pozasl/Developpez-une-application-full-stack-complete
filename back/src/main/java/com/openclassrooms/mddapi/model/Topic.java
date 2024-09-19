403aab57ad7c64008d518b886cf6dbeb25027035
pozasl <loic.pozas@monlaboweb.com>
2024-09-19 17:18:40 +0000

    [MOD] add missing description parameter

--- back/src/main/java/com/openclassrooms/mddapi/model/Topic.java
+++ back/src/main/java/com/openclassrooms/mddapi/model/Topic.java
@@ -7,21 +7,30 @@ public class Topic {
 
     private String ref;
     private String name;
+    private String description;
+
+    public String getRef() {
+        return ref;
+    }
 
     public void setRef(String ref) {
         this.ref = ref;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
-    
-    public String getRef() {
-        return ref;
+
+    public String getDescription() {
+        return description;
     }
 
-    public String getName() {
-        return name;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
 }
