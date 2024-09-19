b437b2c18bf98857c0b3594d5b64ade0e2850050
pozasl <loic.pozas@monlaboweb.com>
2024-09-19 16:52:03 +0000

    [WIP] better mongo setup

--- .devcontainer/docker-entrypoint-initdb.d/setup.js
+++ .devcontainer/docker-entrypoint-initdb.d/setup.js
@@ -1,63 +1,51 @@
-db=db.getSiblingDB("mdd");
-db.dropDatabase();
+const credentials = [
+  {user: "test", pass: "test", db: "test"},
+  {user: "mdduser", pass: "MddUs3r", db: "mdd"},
+]
 
-db.createCollection("topics",{
-  "validator": {
-     "$jsonSchema": {
-       "bsonType": "object",
-       "title": "Student Object Validation",
-       "required": [
-         "id",
-         "name"
-       ]
-    }
-  }
-});
+const loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.";
 
-db.topics.createIndex({"id": 1}, {"unique": true});
+credentials.forEach(cr => {
+  db=db.getSiblingDB(cr.db);
+  db.dropDatabase();
 
-db.runCommand({insert: "topics", documents:[
-  {id: "java", name:"Java"},
-  {id: "angular", name:"Angular"},
-  {id: "spring-boot", name:"Spring-Boot"},
-  {id: "mongodb", name:"MongoDB"},
-  {id: "typescript", name:"TypeScript"},
-  {id: "javascript", name:"JavaScript"}
-]});
-
-
-dbtest=db.getSiblingDB("test");
-dbtest.dropDatabase();
-
-dbtest.createCollection("topics",{
-    "validator": {
-       "$jsonSchema": {
-         "bsonType": "object",
-         "title": "Student Object Validation",
-         "required": [
-           "id",
-           "name"
-         ]
+  db.createCollection("topics",{
+    "valrefator": {
+      "$jsonSchema": {
+        "bsonType": "object",
+        "title": "Student Object Valrefation",
+        "required": [
+          "ref",
+          "name",
+          "description"
+        ]
       }
     }
   });
+
+  db.topics.createIndex({"ref": 1}, {"unique": true});
+
+  db.runCommand({insert: "topics", documents:[
+    {ref: "java", name:"Java", description: loremIpsum},
+    {ref: "angular", name:"Angular", description: loremIpsum},
+    {ref: "spring-boot", name:"Spring-Boot", description: loremIpsum},
+    {ref: "mongodb", name:"MongoDB", description: loremIpsum},
+    {ref: "typescript", name:"TypeScript", description: loremIpsum},
+    {ref: "javascript", name:"JavaScript", description: loremIpsum}
+  ]});
+
+  try {
+    db.createUser(
+      {
+          user: cr.user,
+          pwd: cr.pass,
+          roles: [
+              { role: "readWrite", db: cr.db }
+          ]
+      })
+    
+  } catch (error) {
+    console.log(cr.user + " already exist on " + cr.db);
+  }
   
-dbtest.topics.createIndex({"id": 1}, {"unique": true});
-
-dbtest.runCommand({insert: "topics", documents:[
-  {id: "java", name:"Java"},
-  {id: "angular", name:"Angular"},
-  {id: "spring-boot", name:"Spring-Boot"},
-  {id: "mongodb", name:"MongoDB"},
-  {id: "typescript", name:"TypeScript"},
-  {id: "javascript", name:"JavaScript"}
-]});
-
-dbtest.createUser(
-{
-    user: "test",
-    pwd: "test",
-    roles: [
-        { role: "readWrite", db: "test" }
-    ]
-})
+});
