088fc3d50572acc00234646d573d68b1bb86984c
pozasl <loic.pozas@monlaboweb.com>
2024-09-20 09:58:25 +0000

    [MOD] Setup get mongouser from env var

--- .devcontainer/docker-entrypoint-initdb.d/setup.js
+++ .devcontainer/docker-entrypoint-initdb.d/setup.js
@@ -1,6 +1,6 @@
 const credentials = [
   { user: "test", pass: "test", db: "test" },
-  { user: "mdduser", pass: "MddUs3r", db: "mdd" },
+  { user: process.env["MONGO_USERNAME"], pass: process.env["MONGO_PASSWORD"], db: "mdd" },
 ]
 
 const loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.";
