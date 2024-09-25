cabbae6db6d238cf3450260011b7858d351848a1
pozasl <loic.pozas@monlaboweb.com>
2024-09-25 15:55:55 +0000

    [ADD] Basic JwtService implementation

---
+++ back/auth-api/src/main/java/com/openclassrooms/mdd/auth_api/service/JwtServiceImpl.java
@@ -0,0 +1,62 @@
+package com.openclassrooms.mdd.auth_api.service;
+
+import java.util.stream.Collectors;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import com.openclassrooms.mdd.auth_api.model.UserDetailEntity;
+
+/**
+ * JWT service implementation.
+ */
+@Service
+public class JwtServiceImpl implements JwtService {
+
+  private final JwtEncoder encoder;
+
+  @Autowired
+  public JwtServiceImpl(JwtEncoder encoder) {
+    this.encoder = encoder;
+  }
+
+  @Override
+  public String generateToken(Authentication auth) {
+    UserDetailEntity userDetails = (UserDetailEntity) auth.getPrincipal();
+    String scope = auth.getAuthorities().stream()
+        .map(GrantedAuthority::getAuthority)
+        .collect(Collectors.joining(" "));
+    return createToken(userDetails, scope, this.encoder);
+  }
+
+  /**
+   * Create a JWT Token.
+   *
+   * @param userDetails the user details
+   * @param scope the user's authorities scope
+   * @param encoder the password encoder
+   * @return a JWT token
+   */
+  public static String createToken(
+    UserDetailEntity userDetails,
+      String scope,
+      JwtEncoder encoder
+  ) {
+    Instant now = Instant.now();
+    JwtClaimsSet claims = JwtClaimsSet.builder()
+        .issuer("MDD")
+        .issuedAt(now)
+        .expiresAt(now.plus(1, ChronoUnit.HOURS))
+        .subject(userDetails.getEmail())
+        .claim("scope", scope)
+        .claim("userId", userDetails.getId())
+        .build();
+    return encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+  }
+}
