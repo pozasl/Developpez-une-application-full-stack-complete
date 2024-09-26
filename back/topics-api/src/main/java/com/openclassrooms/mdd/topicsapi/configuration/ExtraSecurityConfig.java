08047cfb7e9da3cf84fb655e578842f754dab569
pozasl <loic.pozas@monlaboweb.com>
2024-09-26 15:58:37 +0000

    [WIP] common module

---
+++ back/topics-api/src/main/java/com/openclassrooms/mdd/topicsapi/configuration/ExtraSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.openclassrooms.mdd.topicsapi.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan 
+public class ExtraSecurityConfig {
+    
+}
