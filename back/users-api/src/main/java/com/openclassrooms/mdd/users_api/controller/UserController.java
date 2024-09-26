78f8d699f47beae5839c88230a1dd74180970482
pozasl <loic.pozas@monlaboweb.com>
2024-09-26 07:08:06 +0000

    [MOD] Cleaning import

--- back/users-api/src/main/java/com/openclassrooms/mdd/users_api/controller/UserController.java
+++ back/users-api/src/main/java/com/openclassrooms/mdd/users_api/controller/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.openclassrooms.mdd.api.UsersApi;
 import com.openclassrooms.mdd.api.UsersApiDelegate;
 import com.openclassrooms.mdd.api.model.NewUser;
 import com.openclassrooms.mdd.api.model.User;
