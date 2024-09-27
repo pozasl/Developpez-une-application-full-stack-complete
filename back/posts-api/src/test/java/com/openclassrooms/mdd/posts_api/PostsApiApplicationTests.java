bee80d024cb5c4454868300e038a44f34e95835f
pozasl <loic.pozas@monlaboweb.com>
2024-09-27 11:59:58 +0000

    [ADD] Posts API starter code

---
+++ back/posts-api/src/test/java/com/openclassrooms/mdd/posts_api/PostsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.mdd.posts_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
