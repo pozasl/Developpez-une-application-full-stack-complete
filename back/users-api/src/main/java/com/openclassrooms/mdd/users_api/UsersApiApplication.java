4894b671c7823888860672a718df835bb0ec9678
pozasl <loic.pozas@monlaboweb.com>
2024-09-27 09:00:40 +0000

    [MOD] Moved @EnableConfigurationProperties in users API

--- back/users-api/src/main/java/com/openclassrooms/mdd/users_api/UsersApiApplication.java
+++ back/users-api/src/main/java/com/openclassrooms/mdd/users_api/UsersApiApplication.java
@@ -2,12 +2,8 @@ package com.openclassrooms.mdd.users_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-import com.openclassrooms.mdd.common.configuration.RsaKeyProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(RsaKeyProperties.class)
 public class UsersApiApplication {
 
 	public static void main(String[] args) {
--- back/users-api/src/main/java/com/openclassrooms/mdd/users_api/configuration/ExtraSecurityConfiguration.java
+++ back/users-api/src/main/java/com/openclassrooms/mdd/users_api/configuration/ExtraSecurityConfiguration.java
@@ -1,9 +1,13 @@
 package com.openclassrooms.mdd.users_api.configuration;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import com.openclassrooms.mdd.common.configuration.RsaKeyProperties;
+
 @Configuration
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @ComponentScan("com.openclassrooms.mdd.common.configuration")
 public class ExtraSecurityConfiguration {
     
