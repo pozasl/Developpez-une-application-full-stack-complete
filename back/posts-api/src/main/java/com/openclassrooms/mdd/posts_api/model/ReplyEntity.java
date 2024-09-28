a51a5b98cdc8bea71b176201b12c10633f5da911
pozasl <loic.pozas@monlaboweb.com>
2024-09-28 09:00:29 +0000

    [MOD] Should be document for mongo transformer

--- back/posts-api/src/main/java/com/openclassrooms/mdd/posts_api/model/ReplyEntity.java
+++ back/posts-api/src/main/java/com/openclassrooms/mdd/posts_api/model/ReplyEntity.java
@@ -2,6 +2,9 @@ package com.openclassrooms.mdd.posts_api.model;
 
 import java.util.Date;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public record ReplyEntity(
     String content,
     Date date,
