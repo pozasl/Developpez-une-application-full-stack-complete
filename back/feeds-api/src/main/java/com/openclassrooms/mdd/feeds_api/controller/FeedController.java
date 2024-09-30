8df9ae409ff3c79fdfc86d5097afacae481154c2
pozasl <loic.pozas@monlaboweb.com>
2024-09-30 18:42:26 +0000

    [MOD] Post service aggregator

--- back/feeds-api/src/main/java/com/openclassrooms/mdd/feeds_api/controller/FeedController.java
+++ back/feeds-api/src/main/java/com/openclassrooms/mdd/feeds_api/controller/FeedController.java
@@ -1,11 +1,14 @@
 package com.openclassrooms.mdd.feeds_api.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.openclassrooms.mdd.api.FeedsApiDelegate;
 import com.openclassrooms.mdd.api.model.Post;
@@ -14,24 +17,38 @@ import com.openclassrooms.mdd.feeds_api.service.FeedServiceImpl;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @SecurityRequirement(name = "Authorization")
 public class FeedController implements FeedsApiDelegate{
 
     private FeedService feedService;
+    private WebClient webClient;
 
-    FeedController(FeedServiceImpl feedService) {
+    @Value("${service.posts.base-url}")
+    private String postServiceUrl;
+
+    FeedController(FeedServiceImpl feedService, WebClient webClient) {
         this.feedService = feedService;
+        this.webClient = webClient;
     }
 
     @GetMapping("/api/feeds/{userid}")
-    Flux<Post> getUserFeed(@PathVariable Long userid, @AuthenticationPrincipal Jwt jwt) {
+    Flux<Post> getUserFeed(@PathVariable Long userid, @AuthenticationPrincipal Jwt jwt, @RequestHeader("Authorization") String token) {
         if (!jwt.getClaim("userId").equals(userid))
         {
             return Flux.error(new AccessDeniedException("Unauthorized access"));
         }
-        return feedService.findPostByUserId(userid).map(feed -> new Post().id(feed.getPostRef()));
+        return feedService.findPostByUserId(userid)
+        .flatMapSequential(feedPost ->fetchPost(feedPost.getPostRef(), token));
+    }
+
+    private Mono<Post> fetchPost(String postRef, String token) {
+        String url = postServiceUrl + "/" + postRef;
+        return webClient.get().uri(url)
+            .header("Authorization", token)
+            .retrieve().bodyToMono(Post.class);
     }
     
 }
