bf72cd145f422d0bb0b55af55348cb41131e95f9
pozasl <loic.pozas@monlaboweb.com>
2024-09-30 16:16:50 +0000

    [ADD] Feed Repository implementation

---
+++ back/feeds-api/src/test/java/com/openclassrooms/mdd/feeds_api/repository/FeedRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.openclassrooms.mdd.feeds_api.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.TestPropertySource;
+
+import com.openclassrooms.mdd.feeds_api.model.FeedPostEntity;
+
+import reactor.test.StepVerifier;
+
+@Tag("S.I.T.")
+@DataR2dbcTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+public class FeedRepositoryTest {
+    @Autowired
+    private FeedRepository feedRepository;
+    private FeedPostEntity feedPost1;
+    private FeedPostEntity feedPost2;
+
+    @BeforeEach
+    void setup() {
+        feedPost1 = new FeedPostEntity( 2L,"1234567890987654321abcd2");
+        feedPost2 = new FeedPostEntity(2L, "1234567890987654321abcd3");
+    }
+
+    @Test
+    void findByUserId() {
+        feedRepository.findByUserId(2L).as(StepVerifier::create)
+            .consumeNextWith(feed -> assertThat(feed).isEqualTo(feedPost1))
+            .consumeNextWith(feed -> assertThat(feed).isEqualTo(feedPost2))
+            .verifyComplete();
+    }
+
+}
