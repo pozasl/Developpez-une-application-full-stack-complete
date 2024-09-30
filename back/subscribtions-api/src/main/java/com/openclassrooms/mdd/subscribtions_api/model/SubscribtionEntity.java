d46bfb8943ccb57495db2e124a609ae4d6ef72d6
pozasl <loic.pozas@monlaboweb.com>
2024-09-30 09:54:27 +0000

    [MOD] id needed for delete method

--- back/subscribtions-api/src/main/java/com/openclassrooms/mdd/subscribtions_api/model/SubscribtionEntity.java
+++ back/subscribtions-api/src/main/java/com/openclassrooms/mdd/subscribtions_api/model/SubscribtionEntity.java
@@ -2,10 +2,13 @@ package com.openclassrooms.mdd.subscribtions_api.model;
 
 import java.util.Date;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document("subscribtions")
 public record SubscribtionEntity (
+    @Id
+    String id,
     Long userId,
     String topicRef,
     Date date
