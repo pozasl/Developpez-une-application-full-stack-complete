a95b5e22c2f2d09f4181e2f412bc463932ada56e
pozasl <loic.pozas@monlaboweb.com>
2024-09-30 12:26:56 +0000

    [ADD] Common security configuration

---
+++ back/feeds-api/src/main/java/com/openclassrooms/mdd/feeds_api/configuration/ExtraSecurityConfiguration.java
@@ -0,0 +1,14 @@
+package com.openclassrooms.mdd.feeds_api.configuration;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.openclassrooms.mdd.common.configuration.RsaKeyProperties;
+
+@Configuration
+@EnableConfigurationProperties(RsaKeyProperties.class)
+@ComponentScan("com.openclassrooms.mdd.common.configuration")
+public class ExtraSecurityConfiguration {
+    
+}
