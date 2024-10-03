4682374fb1eacacb78351c4fee97f8c9787c9377
pozasl <loic.pozas@wyca.fr>
2024-10-03 18:36:25 +0000

    [MOD] Added logout method

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { SessionService } from './services/session.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'front';
+export class AppComponent{
+  title = 'MDD';
+
+  constructor(
+    private sessionService: SessionService,
+    private router: Router
+  ) {}
+
+  public logout():void {
+    this.sessionService.logOut();
+    this.router.navigate(['']);
+  } 
 }
