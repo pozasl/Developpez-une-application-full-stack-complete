0b651d11cce0069b82bfc892b8db5fc9ec1407e4
pozasl <loic.pozas@wyca.fr>
2024-10-03 11:04:12 +0000

    [ADD] Session service

---
+++ front/src/app/services/session.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { User } from '../core/modules/openapi';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SessionService {
+
+  public logged = false;
+  public user: User | undefined;
+
+  private loggedSubject = new BehaviorSubject<boolean>(this.logged);
+
+  public $logged(): Observable<Boolean> {
+    return this.loggedSubject.asObservable();
+  }
+
+  public logIn(user: User):void {
+    this.user = user;
+    this.logged = true;
+    this.next();
+  }
+  public logOut():void {
+    this.user = undefined;
+    this.logged = false;
+    this.next();
+  }
+
+  private next() {
+    this.loggedSubject.next(this.logged);
+  }
+
+}
