d9bf8bd4f521010f34fc572a068e6002b8879acd
pozasl <loic.pozas@wyca.fr>
2024-10-04 08:00:48 +0000

    [MOD] Add navigation toolbar

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,22 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SessionService } from './services/session.service';
-import { Router } from '@angular/router';
+import { NavigationStart, Router } from '@angular/router';
+import { filter, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
+  public $logged!: Observable<Boolean>;
+  public $showNav!: Observable<Boolean>;
   title = 'MDD';
 
   constructor(
     private sessionService: SessionService,
-    private router: Router
+    private router: Router,
   ) {}
+  ngOnInit(): void {
+    this.$logged = this.sessionService.$logged();
+    this.$showNav = this.router.events.pipe(
+      filter(e => e instanceof NavigationStart),
+      map(e => e.url != '/'));
+  }
 
   public logout():void {
     this.sessionService.logOut();
     this.router.navigate(['']);
   } 
+  
 }
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -15,6 +16,7 @@ import { jwtInterceptor } from './interceptors/jwt.interceptor';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatToolbarModule
   ],
   providers: [
     provideHttpClient(withInterceptors([jwtInterceptor])),
