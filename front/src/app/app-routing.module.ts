6e3b64ea6703c9e9d53fe76df38d2af125f26385
pozasl <loic.pozas@wyca.fr>
2024-10-04 08:03:03 +0000

    [MOD] add posts and me routes

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -7,12 +7,16 @@ import { RegisterComponent } from './pages/register/register.component';
 import { AuthGuard } from './guards/auth.guards';
 import { UnauthGuard } from './guards/unauth.guards';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { MeComponent } from './pages/me/me.component';
+import { PostsComponent } from './pages/posts/posts.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [UnauthGuard] },
   { path: 'login', component: LoginComponent, canActivate: [UnauthGuard] },
   { path: 'register', component: RegisterComponent, canActivate: [UnauthGuard] },
   { path: 'topics', component: TopicsComponent,  canActivate: [AuthGuard] },
+  { path: 'posts', component: PostsComponent,  canActivate: [AuthGuard] },
+  { path: 'me', component: MeComponent,  canActivate: [AuthGuard] },
   { path: '404', component: NotFoundComponent},
   { path: '**', redirectTo: '404'}
 ];
