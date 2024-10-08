dd7689a1fe41959a57e277aa537dd61411555ece
pozasl <loic.pozas@monlaboweb.com>
2024-10-08 15:35:25 +0000

    [MOD] Add find by desc and asc ordering in feedpost repo

--- back/feeds-api/src/test/java/com/openclassrooms/mdd/feeds_api/repository/FeedRepositoryTest.java
+++ back/feeds-api/src/test/java/com/openclassrooms/mdd/feeds_api/repository/FeedRepositoryTest.java
@@ -27,18 +27,36 @@ public class FeedRepositoryTest {
 
     private FeedPostEntity feedPost1;
     private FeedPostEntity feedPost2;
-    private LocalDateTime date;
+    private LocalDateTime date1;
+    private LocalDateTime date2;
 
     @BeforeEach
     void setup() {
-        date = LocalDateTime.of(2016, 6, 22, 19, 10, 25);
-        feedPost1 = new FeedPostEntity( 2L,"1234567890987654321abcd2", date);
-        feedPost2 = new FeedPostEntity(2L, "1234567890987654321abcd3", date);
+        date1 = LocalDateTime.of(2016, 6, 22, 19, 10, 25);
+        date2 = LocalDateTime.of(2016, 6, 22, 19, 10, 26);
+        feedPost1 = new FeedPostEntity( 2L,"1234567890987654321abcd2", date1);
+        feedPost2 = new FeedPostEntity(2L, "1234567890987654321abcd3", date2);
     }
 
     @Test
-    void findByUserId() {
+    void findByUserId_shoulReturnFeedPosts() {
         feedRepository.findByUserId(2L).as(StepVerifier::create)
+            .consumeNextWith(feed -> assertThat(feed).isEqualTo(feedPost1))
+            .consumeNextWith(feed -> assertThat(feed).isEqualTo(feedPost2))
+            .verifyComplete();
+    }
+
+    @Test
+    void findByUserIdOrderByCreatedAtDesc_shoulReturnFeedPostsByCreatedAtDesc() {
+        feedRepository.findByUserIdOrderByCreatedAtDesc(2L).as(StepVerifier::create)
+            .consumeNextWith(feed -> assertThat(feed).isEqualTo(feedPost2))
+            .consumeNextWith(feed -> assertThat(feed).isEqualTo(feedPost1))
+            .verifyComplete();
+    }
+
+    @Test
+    void findByUserIdOrderByCreatedAtDesc_shoulReturnFeedPosts() {
+        feedRepository.findByUserIdOrderByCreatedAtAsc(2L).as(StepVerifier::create)
             .consumeNextWith(feed -> assertThat(feed).isEqualTo(feedPost1))
             .consumeNextWith(feed -> assertThat(feed).isEqualTo(feedPost2))
             .verifyComplete();
