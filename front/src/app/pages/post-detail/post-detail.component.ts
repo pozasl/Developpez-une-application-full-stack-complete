f86c62950bd8541215a6ef4218594a92d944fdf0
pozasl <loic.pozas@monlaboweb.com>
2024-10-09 15:44:27 +0000

    [WIP] PostDetail design

--- front/src/app/pages/post-detail/post-detail.component.ts
+++ front/src/app/pages/post-detail/post-detail.component.ts
@@ -2,6 +2,7 @@
 import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -18,7 +19,7 @@ import { SessionService } from 'src/app/services/session.service';
 @Component({
   selector: 'app-post-detail',
   standalone: true,
-  imports: [FormsModule, MatCardModule, ReactiveFormsModule, RouterLink, MatInputModule, MatFormFieldModule, MatIconModule, MatSelectModule, DatePipe],
+  imports: [FormsModule, MatCardModule, ReactiveFormsModule, RouterLink, MatInputModule, MatFormFieldModule, MatIconModule, MatButtonModule,  MatSelectModule, DatePipe],
   templateUrl: './post-detail.component.html',
   styleUrl: './post-detail.component.scss'
 })
