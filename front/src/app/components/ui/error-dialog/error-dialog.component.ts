1277db6e7da5bbe5cd0c0f29721aec1ee5fa2d9e
pozasl <loic.pozas@wyca.fr>
2024-10-11 15:12:13 +0000

    [ADD] ErrorDialog

---
+++ front/src/app/components/ui/error-dialog/error-dialog.component.ts
@@ -0,0 +1,21 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-error-dialog',
+  standalone: true,
+  imports: [MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './error-dialog.component.html',
+  styleUrl: './error-dialog.component.scss'
+})
+export class ErrorDialogComponent {
+  data = inject(MAT_DIALOG_DATA);
+}
