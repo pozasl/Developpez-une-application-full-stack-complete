b002e97fb482355820bae8606ea88eaca802f545
pozasl <loic.pozas@wyca.fr>
2024-10-11 05:25:23 +0000

    [FIX] Unauth Guard

--- front/src/app/guards/unauth.guards.ts
+++ front/src/app/guards/unauth.guards.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, Router, RouterStateSnapshot } from "@angular/router";
 import { SessionService } from "../services/session.service";
-import { catchError, map, of, take } from "rxjs";
+import { catchError, map, of, skip, take } from "rxjs";
 
 @Injectable({ providedIn: 'root' })
 export class UnauthGuard implements CanActivate {
@@ -12,7 +12,10 @@ export class UnauthGuard implements CanActivate {
   ) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<GuardResult> {
+    const skipNbr = this.sessionService.resuming ? 1 : 0
+    console.log("auth");
     return this.sessionService.$logged().pipe(
+      skip(skipNbr),
       take(1),
       map(isLogged => {
         if (isLogged) {
