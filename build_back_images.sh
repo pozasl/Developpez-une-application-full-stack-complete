8bfb8b42f535468ad9a67d415a4c6e55214f4c7b
pozasl <loic.pozas@wyca.fr>
2024-10-12 11:10:27 +0000

    [MOD] Add frontend image build

--- build_back_images.sh
+++ build_back_images.sh
@@ -5,3 +5,4 @@ docker build -f resources/docker/users-api/Dockerfile -t mdd-users-api:0.0.1-SNA
 docker build -f resources/docker/posts-api/Dockerfile -t mdd-posts-api:0.0.1-SNAPSHOT .
 docker build -f resources/docker/subs-api/Dockerfile -t mdd-subs-api:0.0.1-SNAPSHOT .
 docker build -f resources/docker/feeds-api/Dockerfile -t mdd-feeds-api:0.0.1-SNAPSHOT .
+docker build -f resources/docker/front/Dockerfile -t mdd-front-end:0.0.1 .
