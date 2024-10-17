5b3c56d0f58cdf6149ca29f6ec8d16f0886c4730
pozasl <loic.pozas@wyca.fr>
2024-10-17 18:44:15 +0000

    [MOD] Missing Topics component tests

--- front/src/app/pages/topics/topics.component.spec.ts
+++ front/src/app/pages/topics/topics.component.spec.ts
@@ -1,10 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TopicsComponent } from './topics.component';
-import { of } from 'rxjs';
-import { SubscribtionsService, TopicsService } from 'src/app/core/modules/openapi';
-import { provideHttpClient } from '@angular/common/http';
+import { of, take, throwError } from 'rxjs';
+import { ResponseMessage, SubscribtionsService, TopicsService } from 'src/app/core/modules/openapi';
+import { HttpErrorResponse, provideHttpClient } from '@angular/common/http';
 import { SessionService } from 'src/app/services/session.service';
+import { NotificationService } from 'src/app/services/notification.service';
 
 describe('TopicsComponent', () => {
   let component: TopicsComponent;
@@ -34,38 +35,127 @@ describe('TopicsComponent', () => {
 
   const subsServiceStub = {
     getUserSubscribtions: jasmine.createSpy('getUserSubscribtions'),
+    subscribeToTopic: jasmine.createSpy('subscribeToTopic'),
   }
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ TopicsComponent ],
-      providers: [
-        { provide: TopicsService, useValue: topicServiceStub},
-        { provide: SubscribtionsService, useValue: subsServiceStub},
-        { provide: SessionService, useValue: sessionServiceStub},
-        provideHttpClient()
-      ],
+  const notifserviceStub = {
+    notifyError: jasmine.createSpy('notifyError')
+  };
+
+  const error = new HttpErrorResponse({ status: 404 });
+  const response: ResponseMessage = { message: "Ok" };
+
+  describe('when getAllTopics succeeded', () => {
+
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        imports: [TopicsComponent],
+        providers: [
+          { provide: TopicsService, useValue: topicServiceStub },
+          { provide: SubscribtionsService, useValue: subsServiceStub },
+          { provide: SessionService, useValue: sessionServiceStub },
+          { provide: NotificationService, useValue: notifserviceStub },
+          provideHttpClient()
+        ],
+      })
+        .compileComponents();
+      topicServiceStub.getAllTopics.and.returnValue(of([topic1, topic2]));
+      subsServiceStub.getUserSubscribtions.and.returnValue(of([topic1]));
+      fixture = TestBed.createComponent(TopicsComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    afterAll(() => {
+      topicServiceStub.getAllTopics.calls.reset();
+      subsServiceStub.getUserSubscribtions.calls.reset()
     })
-    .compileComponents();
-    topicServiceStub.getAllTopics.and.returnValue(of([topic1, topic2]))
-    subsServiceStub.getUserSubscribtions.and.returnValue(of([topic1, topic2]))
-    fixture = TestBed.createComponent(TopicsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should have topics', () => {
+      component.$topics.pipe(take(1)).subscribe({
+        next: topics => expect(topics).toEqual([topic1, topic2])
+      });
+    });
+
+    it('should fetch Topics and map subscribed ones', () => {
+      const map = new Map<string, boolean>();
+      map.set("java", false)
+      map.set("angular", true)
+      expect(topicServiceStub.getAllTopics).toHaveBeenCalled();
+      expect(subsServiceStub.getUserSubscribtions).toHaveBeenCalledWith(1);
+      expect(component.topicSubs).toEqual(map);
+    });
+
+    it('subscribeToTopic should subscribe to Topic', () => {
+      subsServiceStub.subscribeToTopic.and.returnValue(of(response))
+      component.subscribeToTopic('angular');
+      expect(topicServiceStub.getAllTopics).toHaveBeenCalled();
+      expect(subsServiceStub.getUserSubscribtions).toHaveBeenCalledWith(1);
+      expect(subsServiceStub.subscribeToTopic).toHaveBeenCalledWith(1, 'angular');
+    });
+
+    it('when subscribtion fails subscribeToTopic should notify error', () => {
+      subsServiceStub.subscribeToTopic.and.returnValue(throwError(() => error))
+      component.subscribeToTopic('angular');
+      expect(notifserviceStub.notifyError).toHaveBeenCalledWith("Erreur d'inscription", error.message);
+    });
+
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('when getAllTopics fails', () => {
+
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        imports: [TopicsComponent],
+        providers: [
+          { provide: TopicsService, useValue: topicServiceStub },
+          { provide: SubscribtionsService, useValue: subsServiceStub },
+          { provide: SessionService, useValue: sessionServiceStub },
+          { provide: NotificationService, useValue: notifserviceStub },
+          provideHttpClient()
+        ],
+      })
+        .compileComponents();
+      topicServiceStub.getAllTopics.and.returnValue(throwError(() => error));
+      subsServiceStub.getUserSubscribtions.and.returnValue(of([topic1]));
+      fixture = TestBed.createComponent(TopicsComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should notify error', () => {
+      expect(notifserviceStub.notifyError).toHaveBeenCalledWith("Erreur de chargement", error.message);
+    });
   });
 
-  it('should have topics', () => {
-    expect(component.$topics).toBeTruthy();
+  describe('when getUserSubscribtions fails', () => {
+
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        imports: [TopicsComponent],
+        providers: [
+          { provide: TopicsService, useValue: topicServiceStub },
+          { provide: SubscribtionsService, useValue: subsServiceStub },
+          { provide: SessionService, useValue: sessionServiceStub },
+          { provide: NotificationService, useValue: notifserviceStub },
+          provideHttpClient()
+        ],
+      })
+        .compileComponents();
+      topicServiceStub.getAllTopics.and.returnValue(of([topic1, topic2]));
+      subsServiceStub.getUserSubscribtions.and.returnValue(throwError(() => error));
+      fixture = TestBed.createComponent(TopicsComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should notify error', () => {
+      expect(notifserviceStub.notifyError).toHaveBeenCalledWith("Erreur de chargement", error.message);
+    });
   });
 
-  it('at initialization it should fetch Topics', () => {
-    component.ngOnInit();
-    fixture.detectChanges();
-    expect(topicServiceStub.getAllTopics).toHaveBeenCalled();
-    expect(subsServiceStub.getUserSubscribtions).toHaveBeenCalledWith(1);
-  });
 });
