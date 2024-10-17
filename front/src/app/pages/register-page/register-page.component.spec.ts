1479cf09d234cd61da0040e18f10008b9e4d2125
pozasl <loic.pozas@wyca.fr>
2024-10-17 18:02:25 +0000

    [MOD] Missing RegisterPage component tests

--- front/src/app/pages/register-page/register-page.component.spec.ts
+++ front/src/app/pages/register-page/register-page.component.spec.ts
@@ -1,14 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterPageComponent } from './register-page.component';
-import { provideHttpClient } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
-import { of } from 'rxjs';
+import { HttpErrorResponse, provideHttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of, throwError } from 'rxjs';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthService, NewUser, ResponseMessage } from 'src/app/core/modules/openapi';
+import { NotificationService } from 'src/app/services/notification.service';
 
 describe('RegisterPageComponent', () => {
   let component: RegisterPageComponent;
   let fixture: ComponentFixture<RegisterPageComponent>;
+
+  const routerMock = {
+    navigate: jasmine.createSpy('navigate')
+  };
+
+  const authServiceStub = {
+    register: jasmine.createSpy('register')
+  };
+
+  const notifserviceStub = {
+    notifyError: jasmine.createSpy('notifyError')
+  };
+
+  const bob:NewUser = {
+    name: "Bob",
+    email: "bob@test.com",
+    password: "pass1234"
+  }
 
   const mockRoute = {
     snapshot: {
@@ -21,8 +41,12 @@ describe('RegisterPageComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [RegisterPageComponent, NoopAnimationsModule],
-      providers: [provideHttpClient(),
+      providers: [
+        provideHttpClient(),
         { provide: ActivatedRoute, useValue: mockRoute },
+        { provide: Router, useValue: routerMock },
+        { provide: AuthService, useValue: authServiceStub },
+        { provide: NotificationService, useValue: notifserviceStub },
       ]
     });
     fixture = TestBed.createComponent(RegisterPageComponent);
@@ -33,4 +57,25 @@ describe('RegisterPageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('submit should register', () => {
+    const response: ResponseMessage = { message: "Ok" };
+    authServiceStub.register.and.returnValue(of(response));
+    component.form.setValue(bob);
+    component.submit();
+    fixture.detectChanges();
+    expect(authServiceStub.register).toHaveBeenCalledWith(bob);
+    expect(routerMock.navigate).toHaveBeenCalledWith(['/login']);
+  });
+
+  it('submit should register', () => {
+    const error = new HttpErrorResponse({ status: 409 });
+    authServiceStub.register.and.returnValue(throwError(() => error));
+    component.form.setValue(bob);
+    component.submit();
+    fixture.detectChanges();
+    expect(authServiceStub.register).toHaveBeenCalledWith(bob);
+    expect(notifserviceStub.notifyError).toHaveBeenCalledWith("Erreur d'enregistrement", error.message);
+  });
+  
 });
