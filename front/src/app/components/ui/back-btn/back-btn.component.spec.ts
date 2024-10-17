8762bbaa453cbba2f2e709084a341f441a9a43d6
pozasl <loic.pozas@wyca.fr>
2024-10-17 09:58:20 +0000

    [MOD] Missing BackBtn UT

--- front/src/app/components/ui/back-btn/back-btn.component.spec.ts
+++ front/src/app/components/ui/back-btn/back-btn.component.spec.ts
@@ -1,14 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BackBtnComponent } from './back-btn.component';
+import { Router } from '@angular/router';
 
 describe('BackBtnComponent', () => {
   let component: BackBtnComponent;
   let fixture: ComponentFixture<BackBtnComponent>;
 
+  const routerMock = {
+    navigate: jasmine.createSpy('navigate')
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BackBtnComponent]
+      imports: [BackBtnComponent],
+      providers : [
+        { provide: Router, useValue: routerMock }
+      ]
     })
     .compileComponents();
 
@@ -20,4 +28,19 @@ describe('BackBtnComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('without history goBack should navigate to /', () => {
+    spyOnProperty(window.history, 'length').and.returnValue(1);
+    component.goBack();
+    expect(routerMock.navigate).toHaveBeenCalledWith(['/']);
+    routerMock.navigate.calls.reset();
+  });
+
+  it('with history goBack should goBack in history', () => {
+    spyOnProperty(window.history, 'length').and.returnValue(2);
+    spyOn(window.history, "back");
+    component.goBack();
+    expect(window.history.back).toHaveBeenCalled();
+  });
+
 });
